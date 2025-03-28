70f9e50bb4d16782a711c8777606ec77c6bb5adf
Alejandro Gomez Gambin <alejandro.gomez-gambin@cern.ch>
2025-03-28 16:51:21 +0100

    Quartz sync: Mar 28, 2025, 4:51 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Firmware Development on OS",
+    pageTitleSuffix: "NANO - Microelectronics Master - Microprocessors",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
